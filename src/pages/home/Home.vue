8f9297a204e3bcd9aaddc874c821a332a288048e
yesongsheng@163.com <yesongsheng@163.com>
2018-10-03 10:01:58 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+        <div>
+            Hello World
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style lang="css">
+</style>
