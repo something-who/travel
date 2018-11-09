126ac63a9a8e5e6b99bcb6c571dc460bdfbd8deb
DaYePython <yesongsheng1984240775@gmail.com>
2018-11-09 22:50:35 +0800

    Add Alphabet switch function

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,36 +3,70 @@
     
         <city-header></city-header>
     
-        <city-search></city-search>
+        <city-search :cities="cities"></city-search>
     
-        <city-List></city-List>
+        <city-List
+            :cities="cities"
+            :hot="hotCities"
+            :letter="letter"
+        >
+        </city-List>
+        <city-alphabet
+            :cities="cities"
+            @change="handleChange"
+        ></city-alphabet>
     
     </div>
 </template>
 
 <script>
+    
+    import axios from "axios"
     import CityHeader from './components/Header'
     
     import CitySearch from './components/Search'
     
     import CityList from './components/List'
     
-    
+    import CityAlphabet from './components/Alphabet'
     
     export default {
-    
         name: "City",
-    
+        data (){
+            return{
+                cities : {},
+                hotCities : [],
+                letter : ''
+            }
+        },
         components: {
-    
             CityHeader,
-    
             CitySearch,
-    
-            CityList
-    
+            CityList,
+            CityAlphabet
+        },
+        methods:{
+            getCitiesInfo(){
+                axios.get("/api/city.json")
+                    .then(this.handleCityData)
+            },
+            handleCityData(res){
+                res = res.data
+                if(res.ret && res.data){
+                    const data = res.data
+                    // console.log(data)
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                }
+            },
+            handleChange(e){
+                // console.log(e)
+                this.letter = e
+            }
+        },
+        mounted () {
+          this.getCitiesInfo()  
         }
-    
     }
 </script>
 
